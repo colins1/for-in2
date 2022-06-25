759f1b971a71ed7550ed228c3d2fc080730ba140
colins1 <maxrozhkov1@gmail.com>
2022-06-25 19:49:02 +0300

    add

--- src/js/sort.js
+++ src/js/sort.js
@@ -1,28 +1,14 @@
-export default function orderByProps(obj, arr) {
-    const {name, level, ...rest} = obj
-
-    class Sort {
-        constructor (name, level) {
-            this.name = name
-            this.level = level
+export default function orderByProps(obj) {
+    const {special} = obj
+    for (let i = 0; i < special.length; i++) {
+        for (const description in special[i]) {
+            if (!special[i].description) {
+                special[i].description = 'Описание недоступно';
+            }
         }
     }
-    
-    const sort = new Sort(obj[arr[0]], obj[arr[1]])
-    const ordered = {};
-    Object.keys(rest).sort().forEach(function(key) {
-        ordered[key] = rest[key];
-    });
-
-    const objectAssign = Object.assign(sort, ordered);
-    const objecResult = []
-    for (let key in objectAssign) {
-        objecResult.push(
-            {key: key, value: objectAssign[key]}, // порядок взят из массива с ключами
-        )
-    }
-
-    return objecResult;
+    //console.log(special);
+    return special;
 
 
 
--- src/test/basic.test.js
+++ src/test/basic.test.js
@@ -1,16 +1,22 @@
 import character from '../js/date';
 import orderByProps from '../js/sort';
 
-
+const special = [
+  {
+    id: 8,
+    name: 'Двойной выстрел',
+    icon: 'http://...',
+    description: 'Двойной выстрел наносит двойной урон'
+  }, 
+  {
+    id: 9,
+    name: 'Нокаутирующий удар',
+    icon: 'http://...',
+    description: 'Описание недоступно'
+  }
+]
 
 test('name check < 2', () => {
-  expect(orderByProps(obj, ["name", "level"])
-  ).toEqual([
-    {key: "name", value: "мечник"}, // порядок взят из массива с ключами
-    {key: "level", value: 2}, // порядок взят из массива с ключами
-    {key: "attack", value: 80}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
-    {key: "defence", value: 40}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
-    {key: "health", value: 10} // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
-  ]);
+  expect(orderByProps(character)
+  ).toEqual(special);
 });
-
